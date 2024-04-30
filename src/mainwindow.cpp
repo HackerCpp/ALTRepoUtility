112f74b4884a98c5655de4b4b394735da7c50418
Aleksey Pryanikov <sarlift@mail.ru>
2024-04-30 20:53:58 +0400

    Added packages comparison widget Signed-off-by: Aleksey Pryanikov <sarlift@mail.ru>

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -2,6 +2,11 @@
 #include "./ui_mainwindow.h"
 #include "QVBoxLayout"
 #include <QSplitter>
+#include <QTableView>
+#include <QComboBox>
+#include <QPushButton>
+#include <future>
+#include <list>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -15,10 +20,91 @@ MainWindow::MainWindow(QWidget *parent)
     m_bpwTwo = new BinaryPackagesWidget({});
     m_splitter->addWidget(m_bpwOne);
     m_splitter->addWidget(m_bpwTwo);
+
+    //Compare
+    m_compareWidget = new QWidget;
+    m_compareLayout = new QVBoxLayout;
+    m_compareView = new QTableView;
+    m_compareBtnUpdate = new QPushButton{"Update"};
+    m_compareCombo = new QComboBox;
+    m_compareCombo->insertItem(0, "yes on the left but not on the right", {});
+    m_compareCombo->insertItem(1, "yes on the right but not on the left", {});
+    m_compareCombo->insertItem(2, "the release of which is larger on the left", {});
+    m_functions.push_back(
+       [&](const altrepoapi::BinaryPackages &one,
+          const altrepoapi::BinaryPackages &two){
+        return missingPackages(one,two);
+    });
+    m_functions.push_back(
+       [&](const altrepoapi::BinaryPackages &one,
+          const altrepoapi::BinaryPackages &two){
+        return missingPackages(two, one);
+    });
+    m_compareModel = new BinaryPackagesModel{{}};
+    m_compareView->setModel(m_compareModel);
+    m_compareWidget->setLayout(m_compareLayout);
+    m_compareLayout->addWidget(m_compareCombo);
+    m_compareLayout->addWidget(m_compareBtnUpdate);
+    m_compareLayout->addWidget(m_compareView);
+    m_splitter->insertWidget(1,m_compareWidget);
+
     m_mainLayout->addWidget(m_splitter);
     m_mainWidget->setLayout(m_mainLayout);
     setCentralWidget(m_mainWidget);
+
+    connect(m_compareBtnUpdate, &QPushButton::released, this, &MainWindow::update);
     show();
+}
+
+void MainWindow::
+update(){
+    m_compareBtnUpdate->setDisabled(true);
+    m_compareBtnUpdate->setText("Calculated...");
+    QCoreApplication::processEvents();
+    auto left = m_bpwOne->packages();
+    auto right = m_bpwTwo->packages();
+    int currentIndex = m_compareCombo->currentIndex();
+    try{
+        m_compareModel->update(m_functions.at(currentIndex)(left,right));
+    }
+    catch(std::out_of_range exe){}
+    catch(...){}
+    m_compareBtnUpdate->setEnabled(true);
+    m_compareBtnUpdate->setText("Update");
+}
+
+auto MainWindow::missingPackages(const altrepoapi::BinaryPackages &one,
+                                 const altrepoapi::BinaryPackages &two)const
+->altrepoapi::BinaryPackages
+{
+    altrepoapi::BinaryPackages result;
+#ifdef MULTITHREAD
+    std::list<std::pair<altrepoapi::BinaryPackages::PackageInfo,std::future<bool>>>futures;
+    for(const auto &pInfo : one.vector()){
+        futures.push_back({pInfo,
+                  std::async(std::launch::async,
+                        [=]()->bool{
+                            auto iter = std::find_if(two.vector().begin(),two.vector().end(),[=](const auto& pi){return pi.name == pInfo.name;});
+                            if(iter == two.vector().end())
+                                return true;
+                            return false;
+                   })
+        });
+    }
+    for(auto &pair : futures){
+        if(!pair.second.get())
+            continue;
+        result << pair.first;
+    }
+#else
+    for(const auto &pInfo : one.vector()){
+            auto finded = std::find_if(two.vector().begin(),two.vector().end(),[=](const auto& pi){return pi.name == pInfo.name;});
+            if(finded != two.vector().end())
+                continue;
+            result << pInfo;
+    }
+#endif
+    return result;
 }
 
 MainWindow::~MainWindow()
--- src/mainwindow.h
+++ src/mainwindow.h
@@ -4,6 +4,8 @@
 #include <QMainWindow>
 #include <libs/AltRepoApi/altrepoapi.h>
 #include "binarypackageswidget.h"
+#include <vector>
+#include <functional>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -15,12 +17,31 @@ class MainWindow : public QMainWindow
     QWidget *m_mainWidget;
     QVBoxLayout *m_mainLayout;
     QSplitter *m_splitter;
+
+    QWidget *m_compareWidget;
+    QVBoxLayout *m_compareLayout;
+    QTableView *m_compareView;
+    QPushButton *m_compareBtnUpdate;
+    QComboBox *m_compareCombo;
+    BinaryPackagesModel *m_compareModel;
+
     BinaryPackagesWidget *m_bpwOne;
     BinaryPackagesWidget *m_bpwTwo;
-    //altrepoapi::AltRepoApi m_altRepoApi;
+    std::vector<
+        std::function<
+            altrepoapi::BinaryPackages(const altrepoapi::BinaryPackages &one,
+                                       const altrepoapi::BinaryPackages &two)
+    >> m_functions;
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+
+public slots:
+    void update();
+public:
+    auto missingPackages(const altrepoapi::BinaryPackages &one,
+                         const altrepoapi::BinaryPackages &two)const
+    ->altrepoapi::BinaryPackages ;
 
 
 private:
